32b37805ca9a5072ea65424903aa7169a8e5b67d
Raqeebullah Hasan <rhasan1@stevens.edu>
2023-12-02 17:57:06 -0500

    added users, authors, and books collections

---
+++ config/mongoCollections.js
@@ -0,0 +1,19 @@
+import { dbConnection } from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+    let _col = undefined;
+
+    return async () => {
+        if (!_col) {
+            const db = await dbConnection();
+            _col = await db.collection(collection);
+        }
+
+        return _col;
+    };
+};
+
+
+export const books = getCollectionFn('books');
+export const authors = getCollectionFn('authors');
+export const users = getCollectionFn('users');
