e4efe5e38a6f6a2ce592b85aad42ff61d2f09653
Raqeebullah Hasan <rhasan1@stevens.edu>
2023-12-15 16:32:59 -0500

    added quering for author_id

--- data/blogs.js
+++ data/blogs.js
@@ -1,17 +1,35 @@
-import { blogs } from "../config/mongoCollections.js";
+import { blogs, users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
-export const createBlog = async (author_id, title, content) => {
+import { dbTool } from "./dbTools.js"
 
-    if (!author_id || ObjectId.isValid(author_id) === false)
-        throw "author id missing or invalid";
+export const createBlog = async (author_email, title, content) => {
+    if (!author_email || typeof author_email !== "string" || author_email.trim().length === 0)
+        throw "author email missing or invalid";
     if (!title || typeof title !== "string" || title.trim().length === 0)
         throw "title missing or invalid";
     if (!content || typeof content !== "string" || content.trim().length === 0)
         throw "content missing or invalid";
 
+    let userCollection = undefined;
+    try {
+        userCollection = await users();
+    } catch (e) {
+        throw "user collection not found";
+    }
+    let author_id = undefined;
+
+    try {
+        author_id = await dbTool(userCollection, "emailAddress", author_email, { _id: 1 })[0];
+    } catch (e) {
+        throw "error in finding author using dbTool";
+    }
+
+    if (!author_id)
+        throw "author not found";
+
     const blogCollection = await blogs();
     const newBlog = {
-        author_id: author_id,
+        author_id: new ObjectId(author_id._id),
         title: title.trim(),
         content: content.trim(),
         date: new Date(),
