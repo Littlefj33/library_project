41fc7926b321dc587d63fbfbbd4b90db611515b8
Frank Orlando <forlando@stevens.edu>
2023-12-15 15:49:38 -0500

    Login form validation and start of registration

---
+++ public/js/register.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Form */
+  let registrationForm = document.getElementById("registration-form");
+
+  /* Inputs */
+  const emailInput = document.getElementById("emailAddressInput");
+  const passwordInput = document.getElementById("passwordInput");
+
+  /* Error */
+  let errorP = document.getElementById("error");
+
+  /* Form Functionality */
+});
