9f78ae81a0832522f9831b8bdd9c0c91833d81dc
Raqeebullah Hasan <rhasan1@stevens.edu>
2023-12-17 21:43:19 -0500

    added the import for the returnBook function

--- routes/books.js
+++ routes/books.js
@@ -2,88 +2,88 @@ import { dbTool } from "../data/dbTools.js";
 import { books, users } from "../config/mongoCollections.js";
 import { Router } from "express";
 import xss from "xss";
-import { addReview, requestBook, approveRequest } from "../data/books.js";
+import { addReview, requestBook, approveRequest, returnBook } from "../data/books.js";
 const router = Router();
 router.route("/")
   .get(async (req, res) => {
-  try {
-    const booksCollection = await books();
-    let booksList = await booksCollection
-      .find(
-        {},
-        {
-          projection: {
-            _id: 1,
-            title: 1,
-            authors: 1,
-            publication_date: 1,
-            summary: 1,
-            language: 1,
-            genres: 1,
-            page_count: 1,
-            isbn: 1,
-            condition_status: 1,
-            liability_cost: 1,
-            total_stock: 1,
-            current_stock: 1,
-            current_borrowers: 1,
-            reviews: 1,
-            comments: 1,
-          },
-        }
-      )
-      .toArray();
-    if (!booksList) throw "ERROR: Could not get all books";
+    try {
+      const booksCollection = await books();
+      let booksList = await booksCollection
+        .find(
+          {},
+          {
+            projection: {
+              _id: 1,
+              title: 1,
+              authors: 1,
+              publication_date: 1,
+              summary: 1,
+              language: 1,
+              genres: 1,
+              page_count: 1,
+              isbn: 1,
+              condition_status: 1,
+              liability_cost: 1,
+              total_stock: 1,
+              current_stock: 1,
+              current_borrowers: 1,
+              reviews: 1,
+              comments: 1,
+            },
+          }
+        )
+        .toArray();
+      if (!booksList) throw "ERROR: Could not get all books";
 
-    const user = req.session.user;
-    const userCollection = await users();
-    let bookIndex = 0;
-    for (const book of booksList) {
-      if (user) {
-        let userInfo = await dbTool(
-          userCollection,
-          "emailAddress",
-          user.emailAddress,
-          { _id: 1 }
-        );
-        let user_requested = false;
-        let user_reviewed = false;
-        for (let borrower of book["current_borrowers"]) {
-          if (borrower.toString() === userInfo[0]["_id"].toString()) {
-            user_requested = true;
+      const user = req.session.user;
+      const userCollection = await users();
+      let bookIndex = 0;
+      for (const book of booksList) {
+        if (user) {
+          let userInfo = await dbTool(
+            userCollection,
+            "emailAddress",
+            user.emailAddress,
+            { _id: 1 }
+          );
+          let user_requested = false;
+          let user_reviewed = false;
+          for (let borrower of book["current_borrowers"]) {
+            if (borrower.toString() === userInfo[0]["_id"].toString()) {
+              user_requested = true;
+            }
           }
-        }
-        for (let review of book["reviews"]) {
-          if (review["authorId"].toString() === userInfo[0]["_id"].toString()) {
-            user_reviewed = true;
+          for (let review of book["reviews"]) {
+            if (review["authorId"].toString() === userInfo[0]["_id"].toString()) {
+              user_reviewed = true;
+            }
           }
-        }
-        if (user_requested) {
-          booksList[bookIndex]["user_requested"] = true;
+          if (user_requested) {
+            booksList[bookIndex]["user_requested"] = true;
+          } else {
+            booksList[bookIndex]["user_requested"] = false;
+          }
+          if (user_reviewed) {
+            booksList[bookIndex]["user_reviewed"] = true;
+          } else {
+            booksList[bookIndex]["user_reviewed"] = false;
+          }
         } else {
           booksList[bookIndex]["user_requested"] = false;
-        }
-        if (user_reviewed) {
-          booksList[bookIndex]["user_reviewed"] = true;
-        } else {
           booksList[bookIndex]["user_reviewed"] = false;
         }
-      } else {
-        booksList[bookIndex]["user_requested"] = false;
-        booksList[bookIndex]["user_reviewed"] = false;
+        bookIndex++;
       }
-      bookIndex++;
-    }
 
-    return res.render("books", { title: "Books", data: booksList });
-  } catch (e) {
-    return res.status(500).render("error", {
-      title: "ERROR Page",
-      error: "Internal Server Error",
-    });
-  }
-})
-.post(async (req, res) => {
+      return res.render("books", { title: "Books", data: booksList });
+    } catch (e) {
+      return res.status(500).render("error", {
+        title: "ERROR Page",
+        error: "Internal Server Error",
+      });
+    }
+  })
+  .post(async (req, res) => {
     const bookId = xss(req.body.bookId);
     const userEmail = xss(req.body.userEmail);
 
