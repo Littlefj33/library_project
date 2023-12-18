6d0750abc6e660e72f9e94478abe01e1c52c1d4c
Frank Orlando <forlando@stevens.edu>
2023-12-17 22:03:59 -0500

    mores stuff

--- routes/books.js
+++ routes/books.js
@@ -14,34 +14,33 @@ const router = Router();
 router
   .route("/")
   .get(async (req, res) => {
-    try {
-      const booksCollection = await books();
-      let booksList = await booksCollection
-        .find(
-          {},
-          {
-            projection: {
-              _id: 1,
-              title: 1,
-              authors: 1,
-              publication_date: 1,
-              summary: 1,
-              language: 1,
-              genres: 1,
-              page_count: 1,
-              isbn: 1,
-              condition_status: 1,
-              liability_cost: 1,
-              total_stock: 1,
-              current_stock: 1,
-              current_borrowers: 1,
-              reviews: 1,
-              comments: 1,
-            },
-          }
-        )
-        .toArray();
-      if (!booksList) throw "ERROR: Could not get all books";
+    const booksCollection = await books();
+    let booksList = await booksCollection
+      .find(
+        {},
+        {
+          projection: {
+            _id: 1,
+            title: 1,
+            authors: 1,
+            publication_date: 1,
+            summary: 1,
+            language: 1,
+            genres: 1,
+            page_count: 1,
+            isbn: 1,
+            condition_status: 1,
+            liability_cost: 1,
+            total_stock: 1,
+            current_stock: 1,
+            current_borrowers: 1,
+            reviews: 1,
+            comments: 1,
+          },
+        }
+      )
+      .toArray();
+    if (!booksList) throw "ERROR: Could not get all books";
     try {
       const booksCollection = await books();
       let booksList = await booksCollection
@@ -128,15 +127,6 @@ router
         bookIndex++;
       }
 
-      return res.render("books", { title: "Books", data: booksList });
-    } catch (e) {
-      return res.status(500).render("error", {
-        title: "ERROR Page",
-        error: "Internal Server Error",
-      });
-    }
-  })
-  .post(async (req, res) => {
       return res.render("books", { title: "Books", data: booksList });
     } catch (e) {
       return res.status(500).render("error", {
