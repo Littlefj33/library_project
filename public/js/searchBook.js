3964adc7d05b6f8b23f54be289afdcc8fe365de5
Yuchen Peng <pengyuchen1997@gmail.com>
2023-12-17 20:13:10 -0500

    fix

--- public/js/searchBook.js
+++ public/js/searchBook.js
@@ -2,24 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchForm = document.querySelector('.search-form');
     const booksSection = document.querySelector('.books-section');
     const booksListItems = booksSection.querySelectorAll('.books-list-item');
-    let errorContainer = $('#error-container')
-    let errorText = errorContainer.find('.text-goes-here')
+    const errorP = document.getElementById("error");
     
     booksListItems.forEach(function (bookItem) {
-        bookItem.style.display = 'none';
+        bookItem.hidden = true;
     });
 
     searchForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        errorContainer.hide()
+        errorP.hidden = true
 
-        const query = document.querySelector('input[name="search"]').value.trim()
+        const query = document.querySelector('input[name="search"]').value.trim().toLowerCase()
         if (!query || query.length <= 1) {
-            errorText.text('Error: The search term should at least be 2 characters.')
-            errorContainer.show()
+            errorP.innerHTML = 'Error: The search term should at least be 2 characters.'
+            errorP.hidden = false
             return
         }
-        query = query.toLowerCase();
         const filter = document.querySelector('select[name="filter"]').value;
 
         filterBooks(query, filter);
@@ -27,20 +25,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function filterBooks(query, filter) {
         booksListItems.forEach(function (bookItem) {
-            bookItem.style.display = 'none';
+            bookItem.hidden = true;
 
             let bookTitle, bookAuthors;
 
             if (filter === 'title') {
                 bookTitle = bookItem.querySelector('.toBookDetails').textContent.toLowerCase();
                 if (bookTitle.includes(query)) {
-                    bookItem.style.display = 'block';
+                    bookItem.hidden = false;
                 }
             } else if (filter === 'author') {
                 const authorListItems = bookItem.querySelectorAll('.authors-list li');
                 bookAuthors = Array.from(authorListItems).map(li => li.textContent.toLowerCase());
                 if (bookAuthors.some(author => author.includes(query))) {
-                    bookItem.style.display = 'block';
+                    bookItem.hidden = false;
                 }
             }
         });
