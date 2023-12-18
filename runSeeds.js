4a310e66dc9eb33995237bc2b54d334a424a33ff
Raqeebullah Hasan <rhasan1@stevens.edu>
2023-12-17 21:26:31 -0500

    fixed a bug and added func: usersBlogsEventsSeed

--- runSeeds.js
+++ runSeeds.js
@@ -1,8 +1,12 @@
 import { booksAuthorsSeed } from "./seed/booksAuthorsSeed.js";
+import { usersBlogsEventsSeed } from "./seed/seedFunction.js";
 
 try {
   await booksAuthorsSeed();
+  await usersBlogsEventsSeed();
   console.log("COMPLETE");
+  process.exit(0);
 } catch (e) {
-  console.log(e);
+  console.error(e);
+  process.exit(1);
 }
