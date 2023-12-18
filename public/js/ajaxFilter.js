2b6519d3c0dc4f7b5c056b631c022ceff72c56d9
Yuchen Peng <pengyuchen1997@gmail.com>
2023-12-18 17:51:15 -0500

    Complete the search func with ajax

--- public/js/ajaxFilter.js
+++ public/js/ajaxFilter.js
@@ -1,119 +1,91 @@
 $(document).ready(function () {
-    const booksSection = $('.books-section')
-    const eventsSection = $('.events-section')
-    const blogsSection = $('.blogs-section')
-    const ra
-    const errorElement = $('.alert')
-    if(booksSection) {
+    const searchForm = $('.search-form');
+    const booksSection = $('.books-section');
+    const eventsSection = $('.events-section');
+    const blogsSection = $('.blogs-section');
+    const errorElement = $('.alert');
 
-
+    if (booksSection.length) {
+        handleSearchFormSubmission('book');
+    }
+    
+    if (eventsSection.length) {
+        handleSearchFormSubmission('event');
+    }
+    
+    if (blogsSection.length) {
+        handleSearchFormSubmission('blog');
     }
 
     function handleSearchFormSubmission(item) {
-        searchShowsForm.submit(function (event) {
-            event.preventDefault()
-            errorElement.hide()
-            errorText.empty()
-            const searchTerm = $('input[name="search"]').val().toLowerCase();
+        searchForm.submit(function (event) {
+            event.preventDefault();
+            errorElement.hide();
+            const searchTerm = $('input[name="search"]').val().trim().toLowerCase();
             const filter = $('select[name="filter"]').val();
-            const itemList = $(`.${item}s-list-item`);
-            if (!searchTerm) {
-                return
+            let itemList;
+            if (item == 'book') {
+                itemList = $(`.${item}s-list-item`);
+            } else {
+                itemList = $(`.${item}-list-item`);
             }
+            
+            
+            if (!searchTerm) {
+                itemList.show();
+                return;
+            }
+            
             if (searchTerm.length > 20) {
                 errorElement.text("Error: The search term should be at most 20 characters.");
-                errorElement.show()
+                errorElement.show();
+                return; 
             }
+            
             $.ajax({
                 method: 'GET',
                 url: `/${item}s/json`
             }).then(function (dataList) {
                 if (!dataList || dataList.length === 0) {
-                    errorElement.text('Error: Fail to get json!')
-                    errorElement.show()
-                } 
-                if (filter == title) {
-
+                    errorElement.text('Error: Fail to get json!');
+                    errorElement.show();
+                    return;
                 }
-                var filteredItems = contentList.filter(item => item[title].includes(keyword));
-                var idList = filteredItems.map(item => {item._id});
+                let filteredItems = [];
+                let idList = [];
 
-                allBookItems.each(function() {
-                    var bookId = $(this).find('.toBookDetails').attr('href').split('/').pop(); // 获取每个书籍条目的 ID
-                    if (knownBookIds.includes(bookId)) {
+                if (filter === 'title') {
+                    filteredItems = dataList.filter(item => item.title.toLowerCase().includes(searchTerm));
+                } else if (filter == 'author') {
+                    if (item == 'book') {
+                        filteredItems = dataList.filter(book => {
+                            if (book.authors && Array.isArray(book.authors)) {
+                                return book.authors.some(author => author.toLowerCase().includes(searchTerm));
+                            }
+                            return false
+                        });
+                    } else if (item == 'blog'){
+                        filteredItems = dataList.filter(user => {
+                            let userName = `${user.author_info.firstName} ${user.author_info.lastName}`
+                            return userName.toLowerCase().includes(searchTerm);
+                        });                      
+                    } else {
+                        filteredItems = dataList.filter(user => {
+                            let userName = `${user.organizer_info.firstName} ${user.organizer_info.lastName}`
+                            return userName.toLowerCase().includes(searchTerm);
+                        });
+                    }
+                }
+                idList = filteredItems.map(item => item._id);          
+                itemList.each(function () {
+                    const id = $(this).attr('id');
+                    if (idList.includes(id)) {
                         $(this).show();
                     } else {
                         $(this).hide();
                     }
                 });
-
-// 使用 map 方法提取所需的属性
-var result = filteredItems.map(item => {
-    return {
-        _id: item._id,
-        title: item.title,
-        author: item.author
-    };
-});
-                tvShowList.empty()
-                results.forEach(item => tvShowList.append(createShowLink(item.show)))
-                tvShowList.show()
-                showDetails.hide()
-                
-            })
-            rootLink.show()
-        })
-    }
-
-
-    const messageElement = $('<div class="message"></div>');
-    messageElement.hide(); 
-    const countMessageElement = $('<p class="count-message"></p>');
-    countMessageElement.hide();
-
-    function renderData(data) {
-        const bookList = $('.books-list-item');
-        dataList.empty();
-        const limitedData = data.slice(0, 100);
-
-        limitedData.forEach(function (item) {
-            const itemElement = $('<div class="item"></div>');
-            const title = $('<h2></h2>');
-            const titleLink = $('<a></a>').attr('href', '/books/' + item._id).text(item.title);
-            title.append(titleLink);
-
-            const authors = $('<p></p>').text('Authors: ' + item.authors.join(', '));
-
-            itemElement.append(title);
-            itemElement.append(authors);
-
-            dataList.append(itemElement);
+            });
         });
-        countMessageElement.text(`Matched ${limitedData.length} books.`);
-        countMessageElement.show();
     }
-
-    $('.search-form').submit(function (event) {
-        event.preventDefault();
-        const query = $('input[name="search"]').val().toLowerCase();
-        const filter = $('select[name="filter"]').val();
-
-        $.ajax({
-            url: '/books/data',
-            method: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                renderData(data);
-
-                messageElement.text('Search completed.');
-                messageElement.show();
-            },
-            error: function (error) {
-                console.error('Error fetching data:', error);
-            }
-        });
-    });
-
-    $('body').prepend(countMessageElement);
-    $('body').prepend(messageElement);
 });
